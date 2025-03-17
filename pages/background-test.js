4bfda19da9fcc46769e8e4bcb63a904886a4efcc
Mitchell Dong <mitch@mitchdong.com>
2025-03-17 16:25:19 -0700

    Cleaned up old test code

--- pages/background-test.js
+++
@@ -1,9 +0,0 @@
-import BackgroundRemovalTest from '../test/components/BackgroundRemovalTest';
-
-export default function BackgroundTestPage() {
-  return (
-    <div className="min-h-screen bg-slate-50 p-4">
-      <BackgroundRemovalTest />
-    </div>
-  );
-} 
